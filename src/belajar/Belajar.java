490bc74fdafa455cdd8d16b61a70cc108a39ec7b
rifkifikri <rifkifikri000@gmail.com>
2021-03-17 21:11:49 -0700

    belajarJava

---
+++ src/belajar/Belajar.java
@@ -0,0 +1,13 @@
+package belajar;
+
+public class Belajar {//nama class harus di awali huruf besar
+	public static void main(String[] args){
+		System.out.print("hello kawan\n");
+		//membuat variabel atau assigment
+		int x=10;
+		int y= 29;
+		System.out.println("nilai x=" + x);
+		System.out.println("nilai y="+x);
+
+	}
+}
